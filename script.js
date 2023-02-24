f7999d1b85c89f9b578b24bf6001e71558421f74
Tamara <99070482+thetamaraelaye@users.noreply.github.com>
2023-02-24 08:02:42 +0100

    change 'var' to 'let'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("navbar");
+    let x = document.getElementById("navbar");
     if (x.className === "nav") {
       x.className += "responsive";
     } else {
